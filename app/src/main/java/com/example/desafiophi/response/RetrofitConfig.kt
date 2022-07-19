cb5fd7f02f1e360b900b1a99e872fcb6b42e37da
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2022-07-19 00:12:15 -0300

    Ajustando URL nas configurações da retrofit

--- app/src/main/java/com/example/desafiophi/response/RetrofitConfig.kt
+++ app/src/main/java/com/example/desafiophi/response/RetrofitConfig.kt
@@ -1,16 +1,15 @@
 package com.example.desafiophi.response
 
 import com.example.desafiophi.api.ExtractService
-import com.example.desafiophi.utils.ExtractConfig
+import com.example.desafiophi.utils.BASE_URL
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-
 class RetrofitConfig {
 
-    companion object{
+    companion object {
         fun getRetrofit(): ExtractService {
             val logging = HttpLoggingInterceptor()
             logging.setLevel(HttpLoggingInterceptor.Level.BODY)
@@ -18,12 +17,11 @@ class RetrofitConfig {
                 .addInterceptor(logging)
                 .build()
 
-            val retrofit = Retrofit.Builder()
+            return Retrofit.Builder()
                 .client(client)
-                .baseUrl(ExtractConfig.URL_BASE)
+                .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build().create(ExtractService::class.java)
-            return retrofit
         }
     }
 }
