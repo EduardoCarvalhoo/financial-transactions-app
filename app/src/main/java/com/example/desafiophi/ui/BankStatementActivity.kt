c41d151e58e6597c8e1c2cf71677c8746652f770
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2022-07-19 00:17:25 -0300

    Adicionando melhorias e adicionando novas funcionalidades de tratamento de erros

--- app/src/main/java/com/example/desafiophi/ui/BankStatementActivity.kt
+++ app/src/main/java/com/example/desafiophi/ui/BankStatementActivity.kt
@@ -10,41 +10,44 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.desafiophi.R
 import com.example.desafiophi.adapter.ExtractDetailsAdapter
 import com.example.desafiophi.databinding.ActivityBankStatementBinding
+import com.example.desafiophi.domain.ExtractDetailItem
 import com.example.desafiophi.response.ExtractItemDetailResponse
 import com.example.desafiophi.response.RetrofitConfig
-import com.example.desafiophi.utils.ExtractConfig
+import com.example.desafiophi.utils.TOKEN
+import com.example.desafiophi.utils.TransferOptions
+import com.example.desafiophi.utils.convertDateToFormat
+import com.example.desafiophi.utils.showAlertDialog
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.IOException
 
 class BankStatementActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityBankStatementBinding
-    private var retrieveId: String? = null
+    private lateinit var extractId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBankStatementBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        setupRecyclerView()
+        extractId = intent.getStringExtra(ID) ?: ""
         configureActionBar()
-        voucherIdDetails()
-
+        getExtractDetails()
     }
 
-    private fun setupRecyclerView(item: List<ExtractItemDetailResponse>) {
-        with(binding.bankStatementRecyclerView) {
+    private fun setupRecyclerView() {
+        binding.bankStatementRecyclerView.apply {
             layoutManager = LinearLayoutManager(
                 this@BankStatementActivity, RecyclerView.VERTICAL, false
             )
             setHasFixedSize(true)
-            adapter = ExtractDetailsAdapter(item)
-            configureProgressBar()
         }
     }
 
     companion object {
         private const val ID = "id"
+        private const val DATE_TO_CONVERT = "dd/MM/yyyy - HH:mm:ss"
         fun getStartIntent(context: Context, id: String?): Intent {
             return Intent(context, BankStatementActivity::class.java).apply {
                 putExtra(ID, id)
@@ -52,11 +55,9 @@ class BankStatementActivity : AppCompatActivity() {
         }
     }
 
-    private fun voucherIdDetails() {
-
-        retrieveId = intent.getStringExtra(ID)
-        val extractService =
-            RetrofitConfig.getRetrofit().getMyStatementDetail(ExtractConfig.TOKEN, retrieveId)
+    private fun getExtractDetails() {
+        binding.bankStatementProgressBar.visibility = View.VISIBLE
+        val extractService = RetrofitConfig.getRetrofit().getMyStatementDetail(TOKEN, extractId)
         val call: Call<ExtractItemDetailResponse> = extractService
 
         call.enqueue(object : Callback<ExtractItemDetailResponse> {
@@ -64,36 +65,64 @@ class BankStatementActivity : AppCompatActivity() {
                 call: Call<ExtractItemDetailResponse>,
                 response: Response<ExtractItemDetailResponse>
             ) {
-                if (response.isSuccessful) {
-                    val item = response.body()
-                    if (item != null) {
-                        setupRecyclerView(listOf(item))
+                binding.bankStatementProgressBar.visibility = View.GONE
+                if (response.isSuccessful && response.body() != null) {
+                    val extractItemDetailResponse = response.body() ?: return
+                    binding.bankStatementRecyclerView.adapter =
+                        ExtractDetailsAdapter(getExtractDetailList(extractItemDetailResponse))
+                } else {
+                    showAlertDialog(getString(R.string.server_error)) {
+                        getExtractDetails()
                     }
                 }
             }
 
-            override fun onFailure(call: Call<ExtractItemDetailResponse>, t: Throwable) {
-
+            override fun onFailure(call: Call<ExtractItemDetailResponse>, throwable: Throwable) {
+                binding.bankStatementProgressBar.visibility = View.GONE
+                if (throwable is IOException) {
+                    showAlertDialog(getString(R.string.no_internet_connection_error)) {
+                        getExtractDetails()
+                    }
+                } else {
+                    showAlertDialog(getString(R.string.generic_error)) {
+                        getExtractDetails()
+                    }
+                }
             }
         })
     }
 
-    private fun configureProgressBar() {
-
-        binding.apply {
-            if (bankStatementRecyclerView.adapter != null) {
-                bankStatementProgressBar.visibility = View.GONE
-            }
-        }
-    }
+    private fun getExtractDetailList(
+        extractItemDetailResponse: ExtractItemDetailResponse
+    ) = listOf(
+        ExtractDetailItem(
+            title = getString(R.string.bank_statement_type_of_movement_text),
+            value = extractItemDetailResponse.description.orEmpty()
+        ),
+        ExtractDetailItem(
+            title = getString(R.string.item_voucher_details_transfer_value_text),
+            value = extractItemDetailResponse.amount.orEmpty()
+        ),
+        ExtractDetailItem(
+            title = TransferOptions.valueOfOrNull(extractItemDetailResponse.tType),
+            value = extractItemDetailResponse.to.orEmpty()
+        ),
+        ExtractDetailItem(
+            title = getString(R.string.item_voucher_details_date_time_text),
+            value = extractItemDetailResponse.createdAt.orEmpty()
+                .convertDateToFormat(DATE_TO_CONVERT) ?: ""
+        ),
+        ExtractDetailItem(
+            title = getString(R.string.item_voucher_details_authentication_text),
+            value = extractItemDetailResponse.authentication.orEmpty()
+        )
+    )
 
     private fun configureActionBar() {
-
-        binding.apply {
-            setSupportActionBar(bankStatementToolbar)
-            supportActionBar?.setDisplayHomeAsUpEnabled(true)
-            supportActionBar?.title = null
-            supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_24)
-        }
+        setSupportActionBar(binding.bankStatementToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = null
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_24)
     }
+
 }
--- app/src/main/java/com/example/desafiophi/ui/MainActivity.kt
+++ app/src/main/java/com/example/desafiophi/ui/MainActivity.kt
@@ -2,131 +2,131 @@ package com.example.desafiophi.ui
 
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.desafiophi.R
 import com.example.desafiophi.adapter.ExtractAdapter
 import com.example.desafiophi.databinding.ActivityMainBinding
-import com.example.desafiophi.response.ExtractItemResponse
 import com.example.desafiophi.response.ExtractListResponse
 import com.example.desafiophi.response.MyBalanceResponse
 import com.example.desafiophi.response.RetrofitConfig
-import com.example.desafiophi.utils.ExtractConfig
+import com.example.desafiophi.utils.TOKEN
+import com.example.desafiophi.utils.showAlertDialog
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-    private var visibilityOption: Boolean = true
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        setupRecyclerView()
         settingBalanceVisibility()
         getBankDetailsMyBalance()
         getBankDetailsMyStatement()
-
-
     }
 
-    private fun setupRecyclerView(result: List<ExtractItemResponse>) {
-
+    private fun setupRecyclerView() {
         with(binding.mainRecyclerView) {
             layoutManager = LinearLayoutManager(
                 this@MainActivity, RecyclerView.VERTICAL, false
             )
             setHasFixedSize(true)
-            adapter = ExtractAdapter(result) { dice ->
-                val intent = BankStatementActivity.getStartIntent(this@MainActivity, dice.id)
-                startActivity(intent)
-
-            }
-            configureProgressBar()
         }
     }
 
     private fun getBankDetailsMyBalance() {
-
-        val extractService = RetrofitConfig.getRetrofit().getMyBalance(ExtractConfig.TOKEN)
+        binding.mainProgressBar.isVisible = true
+        val extractService = RetrofitConfig.getRetrofit().getMyBalance(TOKEN)
         val call: Call<MyBalanceResponse> = extractService
 
         call.enqueue(object : Callback<MyBalanceResponse> {
-
             override fun onResponse(
                 call: Call<MyBalanceResponse>,
                 response: Response<MyBalanceResponse>
             ) {
-                if (response.isSuccessful) {
-                    val myBalance: MyBalanceResponse = response.body()!!
-                    binding.mainBalanceTextView.text = myBalance.amount
-
+                binding.mainProgressBar.isVisible = false
+                if (response.isSuccessful && response.body() != null) {
+                    val myBalance: MyBalanceResponse? = response.body()
+                    binding.mainBalanceTextView.text = myBalance?.amount.orEmpty()
+                } else {
+                    binding.mainBalanceTextView.text = getString(R.string.balance_error_message)
                 }
             }
 
             override fun onFailure(call: Call<MyBalanceResponse>, t: Throwable) {
-
+                binding.mainProgressBar.isVisible = false
+                binding.mainBalanceTextView.text = getString(R.string.balance_error_message)
             }
         })
     }
 
     private fun getBankDetailsMyStatement() {
-
-        val extractService = RetrofitConfig.getRetrofit().getMyStatement(ExtractConfig.TOKEN)
+        binding.mainProgressBar.isVisible = true
+        val extractService = RetrofitConfig.getRetrofit().getMyStatement(TOKEN)
         val call: Call<ExtractListResponse> = extractService
 
         call.enqueue(object : Callback<ExtractListResponse> {
-
             override fun onResponse(
                 call: Call<ExtractListResponse>,
                 response: Response<ExtractListResponse>
             ) {
-                if (response.isSuccessful) {
-                    val result = response.body()?.items
-                    if (result != null) {
-                        setupRecyclerView(result)
-
-                    }
-                }
+                binding.mainProgressBar.isVisible = false
+                handleBankDetailsResponse(response)
             }
 
             override fun onFailure(call: Call<ExtractListResponse>, t: Throwable) {
-
+                binding.mainProgressBar.isVisible = false
+                handleBankDetailsFailure(t)
             }
         })
     }
 
-    private fun configureProgressBar() {
+    private fun handleBankDetailsFailure(throwable: Throwable) {
+        if (throwable is IOException) {
+            showAlertDialog(getString(R.string.no_internet_connection_error)) {
+                getBankDetailsMyStatement()
+            }
+        } else {
+            showAlertDialog(getString(R.string.generic_error)) {
+                getBankDetailsMyStatement()
+            }
+        }
+    }
 
-        binding.apply {
-            if (mainRecyclerView.adapter != null) {
-                mainProgressBar.visibility = View.GONE
+    private fun handleBankDetailsResponse(response: Response<ExtractListResponse>) {
+        if (response.isSuccessful && response.body() != null) {
+            binding.mainRecyclerView.adapter = ExtractAdapter(
+                response.body()?.items ?: return
+            ) { extractItemResponse ->
+                val intent = BankStatementActivity.getStartIntent(
+                    this@MainActivity, extractItemResponse.id
+                )
+                startActivity(intent)
+            }
+        } else {
+            showAlertDialog(getString(R.string.server_error)) {
+                getBankDetailsMyStatement()
             }
         }
     }
 
     private fun settingBalanceVisibility() {
-
         binding.apply {
             mainVisibilityImageButton.setOnClickListener {
-
-                if (visibilityOption) {
-                    mainBalanceTextView.visibility = View.VISIBLE
-                    mainHideBalanceProgressBar.visibility = View.GONE
+                mainBalanceTextView.isVisible = !mainBalanceTextView.isVisible
+                mainHideBalanceProgressBar.isVisible = !mainHideBalanceProgressBar.isVisible
+                if (mainBalanceTextView.isVisible) {
                     mainVisibilityImageButton.setImageResource(R.drawable.ic_baseline_visibility_24)
-                    visibilityOption = false
                 } else {
-                    mainBalanceTextView.visibility = View.GONE
-                    mainHideBalanceProgressBar.visibility = View.VISIBLE
                     mainVisibilityImageButton.setImageResource(R.drawable.ic_visibility_off_24)
-                    visibilityOption = true
                 }
             }
         }
