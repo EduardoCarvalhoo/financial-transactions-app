2d58573664ce74a91734d24737f1380e0804ce2f
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2022-07-19 00:24:13 -0300

    Configurando opções de transferência

--- app/src/main/java/com/example/desafiophi/utils/TransferOptions.kt
+++ app/src/main/java/com/example/desafiophi/utils/TransferOptions.kt
@@ -1,10 +1,20 @@
 package com.example.desafiophi.utils
 
-enum class TransferOptions(val transferOptions: String?) {
+enum class TransferOptions(val value: String) {
     TRANSFEROUT("TRANSFEROUT"), TRANSFERIN("TRANSFERIN"),
     PIXCASHIN("PIXCASHIN"), PIXCASHOUT("PIXCASHOUT"),
-    BANKSLIPCASHIN("BANKSLIPCASHIN"), RECEIVER("Recebedor"),
-    PAYER("Pagador"),
+    BANKSLIPCASHIN("BANKSLIPCASHIN");
 
-
+    companion object {
+        fun valueOfOrNull(transferOption: String?): String? {
+            return when (transferOption) {
+                TRANSFEROUT.value -> TransactionAgent.RECEIVER.value
+                TRANSFERIN.value -> TransactionAgent.PAYER.value
+                PIXCASHIN.value -> TransactionAgent.PAYER.value
+                PIXCASHOUT.value -> TransactionAgent.RECEIVER.value
+                BANKSLIPCASHIN.value -> TransactionAgent.PAYER.value
+                else -> null
+            }
+        }
+    }
 }
