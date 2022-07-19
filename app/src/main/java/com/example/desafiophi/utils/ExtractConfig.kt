63a983f08df15caff0c82181702b98fb0fe4c94d
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2022-07-19 00:22:15 -0300

    Ajustando url e token

--- app/src/main/java/com/example/desafiophi/utils/ExtractConfig.kt
+++ app/src/main/java/com/example/desafiophi/utils/ExtractConfig.kt
@@ -1,10 +1,6 @@
 package com.example.desafiophi.utils
 
-class ExtractConfig {
-
-    companion object{
-        const val URL_BASE = "https://desafio-mobile-bff.herokuapp.com/"
-        const val TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    }
-
-}
+const val BASE_URL = "https://desafio-mobile-bff.herokuapp.com/"
+const val TOKEN =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9l" +
+            "IiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
